8e3f2a717efc4c2f3ea6a9022db43038d1af5d10
Eugene <eugene_chernyshev@yahoo.com>
2026-02-27 15:20:59 +0200

    Update licence.blade.php: Project name
    Added the project name "Solar Power Calculator" to focus on the relative with the project and the MIT licence

--- resources/views/licence.blade.php
+++ resources/views/licence.blade.php
@@ -34,11 +34,32 @@
             .content {
                 text-align: justify;
             }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
         </style>
     </head>
     <body style="display: flex; justify-content: center">
-    <div style="width: 100vh" class="flex-center position-ref full-height">
-            <div class="content">
+    <div style="width: 100vh; position: relative" class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md" style="text-align: center">
+                    Solar Power Calculator
+            </div>
             <h3>MIT Licence</h3>
             <p>Copyright (c) 2026 Eugene</p>
             <p>Permission is hereby granted, free of charge, to any person obtaining a copy
