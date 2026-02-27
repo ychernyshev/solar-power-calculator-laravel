b2fd80bd7576a7e3b00bdff233056d7985805d46
Eugene <eugene_chernyshev@yahoo.com>
2026-02-27 15:01:49 +0200

    MIT Licence
    - Added the MIT licence file to the project
    - Added the MIT licence description in the licence view (template)

--- resources/views/licence.blade.php
+++ resources/views/licence.blade.php
@@ -1,10 +1,64 @@
 <html>
     <head>
         <title>{{ $title }}</title>
-    </head>
-    <body>
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
 
-</body>
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: justify;
+            }
+        </style>
+    </head>
+    <body style="display: flex; justify-content: center">
+    <div style="width: 100vh" class="flex-center position-ref full-height">
+            <div class="content">
+            <h3>MIT Licence</h3>
+            <p>Copyright (c) 2026 Eugene</p>
+            <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:</p>
+            <p>The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.</p>
+            <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</p>
+            </div>
+        </div>
+    </body>
 </html>
 
 
