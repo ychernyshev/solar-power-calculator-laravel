e3a0bb5cad77e0334ae4b565fa4c31929253add5
Eugene <eugene_chernyshev@yahoo.com>
2026-03-04 20:19:35 +0200

    Update licence.blade.php
    Got and showing third-party license information in the cards from the database

--- resources/views/licence.blade.php
+++ resources/views/licence.blade.php
@@ -40,19 +40,19 @@
     <div class="collapse" id="collapseExample">
       <div class="card card-body">
         {{-- Corousell --}}
-        <div id="carouselExampleIndicators" class="carousel slide">
+        <div id="licence_text" class="carousel slide">
           <div class="carousel-indicators">
             @foreach ($licences as $licence)
+              <button type="button"
+              data-bs-target="#licence_text"
+              data-bs-slide-to="{{ $licence->id - 1 }}"
+              class="{{ $licence->id - 1 === 0 ? 'active' : '' }}"
+              aria-current="{{ $licence->id - 1 === 0 ? 'true' : 'false' }}"
+              aria-label="Slide {{ $loop->iteration }}"></button>
               {{-- <button type="button"
               data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0+{{ $licence->id }}"
-              class="active"
-              aria-current="true"
-              aria-label="Slide {{ $licence->id }}"></button> --}}
-              <button type="button"
-              data-bs-target="#carouselExampleIndicators"
               data-bs-slide-to="{{ $licence->id }}"
-              aria-label="Slide {{ $licence->id }}"></button>
+              aria-label="Slide {{ $licence->id }}"></button> --}}
             @endforeach
 
 
@@ -67,8 +67,8 @@
           </div>
           <div class="carousel-inner">
             @foreach ($licences as $licence)
-              <div class="carousel-item active text-left">
-                <h3>Bootstrap</h3>
+              <div class="carousel-item {{ $licence->id - 1 ===0 ? 'active' : '' }} text-left">
+                <h3>{{ $licence->vendor }}</h3>
                 <h5>{{ $licence->type }} License</h5>
                 <p>Copyright (c) {{ $licence->year }} {{ $licence->vendor }}</p>
                 <p style="text-align: justify">Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -88,77 +88,13 @@
                 SOFTWARE.</p>
               </div>
             @endforeach
-
-            {{-- <div class="carousel-item text-left">
-              <h3>Bootswatch</h3>
-              <h5>MIT License</h5>
-              <p>Copyright (c) 2013 Thomas Park</p>
-              <p style="text-align: justify">Permission is hereby granted, free of charge, to any person obtaining a copy
-              of this software and associated documentation files (the "Software"), to deal
-              in the Software without restriction, including without limitation the rights
-              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-              copies of the Software, and to permit persons to whom the Software is
-              furnished to do so, subject to the following conditions:</p>
-              <p style="text-align: justify">The above copyright notice and this permission notice shall be included in all
-              copies or substantial portions of the Software.</p>
-              <p style="text-align: justify">THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-              SOFTWARE.</p>
-            </div>
-            <div class="carousel-item text-left">
-              <h3>Bootstrap Icons</h3>
-              <h5>MIT License</h5>
-              <p>Copyright (c) 2019-2025 The Bootstrap Authors</p>
-              <p style="text-align: justify">Permission is hereby granted, free of charge, to any person obtaining a copy
-              of this software and associated documentation files (the "Software"), to deal
-              in the Software without restriction, including without limitation the rights
-              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-              copies of the Software, and to permit persons to whom the Software is
-              furnished to do so, subject to the following conditions:</p>
-              <p style="text-align: justify">The above copyright notice and this permission notice shall be included in all
-              copies or substantial portions of the Software.</p>
-              <p style="text-align: justify">THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-              SOFTWARE.</p>
-            </div>
-            <div class="carousel-item text-left">
-              <h3>Font Awesome Free</h3>
-              <h5 style="text-decoration: dashed">Font Awesome Free License</h5>
-              <p>------------------------------------</p>
-              <p style="text-align: justify">Font Awesome Free is free, open source, and GPL friendly. You can use it for
-              commercial projects, open source projects, or really almost whatever you want.</p>
-              <h5>MIT License</h5>
-              <p>Copyright (c) 2022 Fonticons, Inc.</p>
-              <p style="text-align: justify">Permission is hereby granted, free of charge, to any person obtaining a copy
-              of this software and associated documentation files (the "Software"), to deal
-              in the Software without restriction, including without limitation the rights
-              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-              copies of the Software, and to permit persons to whom the Software is
-              furnished to do so, subject to the following conditions:</p>
-              <p style="text-align: justify">The above copyright notice and this permission notice shall be included in all
-              copies or substantial portions of the Software.</p>
-              <p style="text-align: justify">THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-              SOFTWARE.</p> --}}
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <button class="carousel-control-prev" type="button" data-bs-target="#licence_text" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <button class="carousel-control-next" type="button" data-bs-target="#licence_text" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
